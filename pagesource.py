977e46992dca02875f4cc0663c6bc68fd2efbded
Alex Cantrell <Alex Cantrell>
2016-07-02 05:32:21 -0500

    Added the Fox News portion of the crawler. Still having unicode issues with it however. This will be requiring more of my attention. In an odd realization, Fox news has code that is much nicer to look at then CNN who kinda failed in the formatting department. The CNN side of the app also needs some work as it randomly cuts out words and paragraphs on a whim. However, for this being one of three projects I'm working on, I feel it's getting underway quite well. I will be focusing more attention on it this weekend however as my job will demand less of my time and my deadline for this is fast approaching.

--- pagesource.py
+++ pagesource.py
@@ -1,7 +1,10 @@
 import urllib2
+import codecs
 def remove_html(string):
     while(string.find("<")!=-1):
         string = string[:string.find("<")]+""+string[string.find(">")+1:]
+    while(string.find("&nbsp;")!=-1):
+        string = string[:string.find("&nbsp;")]+" "+string[string.find("&nbsp;")+6:]
     return string
 while True:
     print"Enter the URL you wish to crawl..."
@@ -9,7 +12,6 @@ while True:
     myurl = input("@>")
     response = urllib2.urlopen(myurl)
     orig_page_source =page_source=response.read()
-    #print(page_source)
     data=""
     if(myurl.find("cnn")!=-1):
         title = page_source[(page_source.find("<h1 class=\"pg-headline\">")+24):page_source.find("</h1>")]
@@ -28,14 +30,25 @@ while True:
             data = data +"\n"+remove_html(page_source[beg:end])
             print(remove_html(page_source[beg:end]))
             page_source = page_source[end+6:]
-         #   print(data)
-        #page_source = page_source[page_source.find("<div class=\"zn-body__paragraph\">")-1:]
-        #data=page_source[page_source.find("<div class=\"zn-body__paragraph\">")+32:page_source.find("</div>")]
-        print("Title: "+title)
-        print("Author: "+author)
-        print("Date: "+date)
-        #print("Data: "+data)
-    #else:
-        
-    #print(data)
+    else:
+        page_source = page_source[page_source.find("articleInfo")+11:]
+        title=page_source[(page_source.find("summary:\"<p>")+12):page_source.find("<\/p>")]
+        author = "N/A"
+        page_source = page_source[page_source.find("pubDate:\"")-1:]
+        date = page_source[page_source.find("pubDate:\"")+8:page_source.find("\",")]
+        page_source = page_source[page_source.find("<div class=\"article-text\">"):]
+        temp_source = page_source[:page_source.find("</div>")]
+        page_source = page_source[page_source.find("</div>")+6:]
+        while(temp_source.find("<p>")!=-1):
+            data = data +"\n"+remove_html(temp_source[temp_source.find("<p>")+3:temp_source.find("</p>")])
+            temp_source = temp_source[temp_source.find("</p>")+4:]
+    try:
+        data = data.encode('UTF-8')
+        #data = data.decode('Latin-1')
+    except:
+        lol = "wat"
+    print("Title: "+title)
+    print("Author: "+author)
+    print("Date: "+date)
+    print("Data: "+data)
     print"Successfully added to the database"
