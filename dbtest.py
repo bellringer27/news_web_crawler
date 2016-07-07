3cd891da44de50ac73650e08b2079576e3c131de
root <root@localhost.localdomain>
2016-07-06 23:18:09 -0500

    Modified dbtest.py in order to make it actually interact with the database. Transferring files over to actual comp instead of vm so I can work with a better editor.

--- dbtest.py
+++ dbtest.py
@@ -1,10 +1,11 @@
 import MySQLdb
 
 
-db = MySQLdb.connect(host="localhost"
-                     user="root"
-                     password="root"
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                     passwd="root",
                      db = "news_web_crawler")
 cur = db.cursor()
-
-cur.execute("INSERT INTO comments (comment_article_id,comment_id,comment_author,comment_data) VALUES('19200','19200','cantrell','lol');")
+cur.execute("INSERT INTO comments (comment_article_id,comment_id,comment_author,comment_data) VALUES('19200','19200','cantrell','lol')")
+db.commit()
+db.close()
