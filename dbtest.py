c0becea323a961463dd97ea121e7e983351cc92b
bellringer27 <bellringer111@gmail.com>
2016-07-06 22:45:35 -0500

    Created Database testing python file in order to attempt to work with the database.

---
+++ dbtest.py
@@ -0,0 +1,10 @@
+import MySQLdb
+
+
+db = MySQLdb.connect(host="localhost"
+                     user="root"
+                     password="root"
+                     db = "news_web_crawler")
+cur = db.cursor()
+
+cur.execute("INSERT INTO comments (comment_article_id,comment_id,comment_author,comment_data) VALUES('19200','19200','cantrell','lol');")
